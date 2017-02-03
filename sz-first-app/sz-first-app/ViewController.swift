83275c66965f7a7c2dc13919d2646fa34f96bdea
Taro Era <era@crypton.co.jp>
2017-02-03 12:00:04 +0900

    initial commit.

---
+++ sz-first-app/sz-first-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sz-first-app
+//
+//  Created by Taro Era on 2017/02/03.
+//  Copyright © 2017年 jp.co.sz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
